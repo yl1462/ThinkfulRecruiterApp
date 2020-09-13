1005c60f019d01a8b03d7cf8e8dc6c9835e7b68e
yl1462 <yliang1462@hotmail.com>
2020-09-13 16:43:32 -0500

    I think everything works... and up to requirements

--- index.js
+++ index.js
@@ -13,25 +13,28 @@ function watchForm() {
 function fetchUserName(userName) {
   console.log("fetching User")
   fetch(`https://api.github.com/users/${userName}/repos`)
-  .then(res => res.json())
-  .then(resJson => showResults(resJson))
-  .catch(error => {
-    console.log(error.message);
-  });
+    .then(res => res.json())
+    .then(resJson => showResults(resJson))
+    .catch(error => {
+      console.log(error.message);
+    });
 }
 
 function showResults(resJson, userName) {
   console.log(resJson);
   let userInfo = `<h2>User:${userName}</h2>`
+  let repoResults = ''
   $('#results').html(userInfo)
   for (let i = 0; i < resJson.length; i++) {
-    $('#results').html(`
-    <li><h4>${resJson[i].name}</h4><br>
-    <a href="$resJson[i].html_url">${resJson[i].html_url}</a><br>
+    repoResults += `<ol><h4>${resJson[i].name}</h4><br>
+    <a href="${resJson[i].clone_url}">${resJson[i].clone_url}</a>
     <p>${resJson[i].description}</p>
-    </li>    
-    `)}
+    <hr>
+    </ol>    
+    `}
+  $('#results').html(repoResults)
 }
+
 
 function errorMessage(error) {
   console.log("errorMessage running");
